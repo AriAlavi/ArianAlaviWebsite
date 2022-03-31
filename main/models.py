d94ef99aa05d8c2600b6ddd7729de8c9d79f791f
AriAlavi <arianalavi1@gmail.com>
2022-03-30 19:47:00 -0700

    Model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField(max_length=128)
+    image = models.ImageField(upload_to='projects')
+
+    def __str__(self):
+        return f"[{self.name}] {self.description}"
