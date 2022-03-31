09946115b684af935b680d0aeb2bc559d24cd8ac
AriAlavi <arianalavi1@gmail.com>
2022-03-30 20:00:45 -0700

    dates

--- main/models.py
+++ main/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Project(models.Model):
     name = models.CharField(max_length=64)
+    startDate = models.DateField()
+    endDate = models.DateField()
     description = models.TextField(max_length=128)
     image = models.ImageField(upload_to='projects')
 
