71ed216fbfe92889e46e905becb90dd4113a2f78
AriAlavi <arianalavi1@gmail.com>
2022-03-30 19:50:32 -0700

    projects loaded

--- main/views.py
+++ main/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.http import FileResponse
+from main.models import *
 
 def home(request):
-    return render(request, "main/home.html")
+    context = {
+        "projects" : Project.objects.all()
+    }
+    return render(request, "main/home.html", context)
 
 def interests(request):
     return render(request, "main/interests.html")
