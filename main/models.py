323416b60f572ad530bff6b4da2f799dd7229582
AriAlavi <arianalavi1@gmail.com>
2022-04-03 18:06:44 -0700

    MORE CHARACTERS

--- main/models.py
+++ main/models.py
@@ -21,7 +21,7 @@ class Project(models.Model):
     name = models.CharField(max_length=64)
     startDate = models.DateField()
     endDate = models.DateField()
-    description = models.TextField(max_length=128)
+    description = models.TextField(max_length=1028)
     image = models.ImageField(upload_to='projects')
     link = models.URLField(default=None, null=True, blank=True)
     skillsUsed = models.ManyToManyField(Skill)
